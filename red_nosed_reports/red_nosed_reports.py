dcdf7f3eee7e104f79fa3d77635eadf5ce3dfb73
Perpetua-Kabute <kabuteperpetua@gmail.com>
2024-12-03 02:20:07 +0300

    feat: solve day 2 red nosed reports

--- red_nosed_reports/red_nosed_reports.py
+++ red_nosed_reports/red_nosed_reports.py
@@ -20,75 +20,39 @@ def is_increasing_or_decreasing_gradually(report):
     is_decreasing = False
     for i in range(len(report)):
         if i == len(report) - 1:
-            break 
+            return (is_decreasing or is_increasing ) and is_differing_at_margin
         if report[i] > report [i+1]:
             is_decreasing = True
             is_differing_at_margin = (1 <= (report[i] - report[i+1]) <= 3)
             if is_increasing or is_differing_at_margin == False: 
-                break
-        else:
-            is_increasing = True
-            is_differing_at_margin = (1 <= (report[i+1] - report[i]) <= 3)
-            if is_decreasing or is_differing_at_margin == False:
-                break
-
-        # if is_differing_at_margin == False:
-        #         break
-    
-    return (is_increasing and is_differing_at_margin and is_decreasing == False) or (is_decreasing and is_differing_at_margin and is_increasing == False)
-
-
-def is_increasing_or_decreasing_gradually_dampened(report, has_removed_element):
-    """
-    param report: list of int - the list of numbers
-    return: Boolean if the list is increasing or decreasing return true
-    """
-    is_increasing = False
-    is_differing_at_margin = False
-    is_decreasing = False
-    for i in range(len(report)):
-        if i == len(report) - 1:
-            break 
-        if report[i] > report [i+1]:
-            is_decreasing = True
-            is_differing_at_margin = (1 <= (report[i] - report[i+1]) <= 3)
-            if is_increasing or is_differing_at_margin == False: 
-                if(has_removed_element):
-                    return False
-                else:
-                    if i == 1 and report[i+1] < report[i+2]:
-                        report.remove(report[0])
-                    else:
-                        report.remove(report[i + 1])
-                    print(report)
-                    return is_increasing_or_decreasing_gradually_dampened(report, True)
-                          
-        elif report[i] < report [i+1] :
-            is_increasing = True
-            is_differing_at_margin = (1 <= (report[i+1] - report[i]) <= 3)
-            if is_decreasing or is_differing_at_margin == False:
-                if(has_removed_element):
-                    return False
-                else:
-                    report.remove(report[i + 1])
-                    print(report)
-                    return is_increasing_or_decreasing_gradually_dampened(report, True)  
-        else:
-            if(has_removed_element):
                 return False
+        else:
+            is_increasing = True
+            is_differing_at_margin = (1 <= (report[i+1] - report[i]) <= 3)
+            if is_decreasing or is_differing_at_margin == False:
+                return False
+    # return (is_increasing and is_differing_at_margin and is_decreasing == False) or (is_decreasing and is_differing_at_margin and is_increasing == False)
+
+def is_gradual_after_removing_number(report):
+    report_copy = report[:]
+
+    if is_increasing_or_decreasing_gradually(report):
+        return True
+    else:
+        for i in range(len(report)):
+            print(f"i = {i} removed = {report_copy[i]}")
+            report_copy.pop(i)
+            if is_increasing_or_decreasing_gradually(report_copy):
+                return True
             else:
-                report.remove(report[i])
-                print(report)
-                return is_increasing_or_decreasing_gradually_dampened(report, True)
-
-        
-                    
-
-    return (is_increasing and is_differing_at_margin and is_decreasing == False) or (is_decreasing and is_differing_at_margin and is_increasing == False)
-
-# def do_second_iteration(report,element_to_remove, has_removed_element):
-#     if(has_removed_element == False):
-#         is_increasing_or_decreasing_gradually_dampened([report.remove(element_to_remove)])
+                print(f"Report was not gradual  at this point. i = {i} report = {report_copy}")
+                report_copy = report[:]
+                
+        return False
+    
+            
+def is_gradual_dampened(report):
+    return is_increasing_or_decreasing_gradually(report) or is_gradual_after_removing_number(report)
 
 
 def find_safe_reports(reports):
@@ -98,10 +62,9 @@ def find_safe_reports(reports):
     
     safe_reports = 0
     for report in reports:
-        if is_increasing_or_decreasing_gradually_dampened(report, False) == True:
+        if is_gradual_dampened(report) == True:
             safe_reports = safe_reports + 1
     return safe_reports
 
 reports = fetch_and_parse_reports()
 print(find_safe_reports(reports))
-# is_increasing_or_decreasing_gradually([8,6,4,4,1])
--- red_nosed_reports/test_red_nosed_reports.py
+++ red_nosed_reports/test_red_nosed_reports.py
@@ -1,49 +1,76 @@
 import unittest
 from red_nosed_reports import find_safe_reports
-from red_nosed_reports import is_increasing_or_decreasing_gradually_dampened
+from red_nosed_reports import is_gradual_dampened
 
 class RedNosedReportsTestCase(unittest.TestCase):
     def test_is_increasing_or_decreasing_gradually(self):
-        result = is_increasing_or_decreasing_gradually_dampened([7,6,4,2,1], False)
+        result = is_gradual_dampened([7,6,4,2,1])
         expected = True
         self.assertEqual(result, expected)
 
     def test_is_increasing_gradually_second(self):
-        result = is_increasing_or_decreasing_gradually_dampened([1,2,7,8,9], False)
+        result = is_gradual_dampened([1,2,7,8,9])
         expected = False
         self.assertEqual(result, expected)
 
     def test_is_increasing_gradually_third(self):
-        result = is_increasing_or_decreasing_gradually_dampened([9,7,6,2,1], False)
+        result = is_gradual_dampened([9,7,6,2,1])
         expected = False
         self.assertEqual(result, expected)
 
     def test_is_increasing_gradually_fourth(self):
-        result = is_increasing_or_decreasing_gradually_dampened([1,3,2,4,5], False)
+        result = is_gradual_dampened([1,3,2,4,5])
         expected = True
         self.assertEqual(result, expected)
 
     def test_is_increasing_gradually_fifth(self):
-        result = is_increasing_or_decreasing_gradually_dampened([8,6,4,4,1], False)
+        result = is_gradual_dampened([8,6,4,4,1])
         expected = True
         self.assertEqual(result, expected)
 
     def test_is_increasing_gradually_sixth(self):
-        result = is_increasing_or_decreasing_gradually_dampened([1,3,6,7,9], False)
+        result = is_gradual_dampened([1,3,6,7,9])
         expected = True
         self.assertEqual(result, expected)
     def test_increasing_last(self):
-        result = is_increasing_or_decreasing_gradually_dampened([76,79,80,83,85,86,88], False)
+        result = is_gradual_dampened([76,79,80,83,85,86,88])
         expeced = True
         self.assertEqual(result,expeced)
 
     def test_increasing_remove_first(self):
-        result = is_increasing_or_decreasing_gradually_dampened([55,52,53,54,56,57], False)
+        result = is_gradual_dampened([55,52,53,54,56,57])
         expeced = True
         self.assertEqual(result,expeced)
 
     def test_decreasing_remove_first(self):
-        result = is_increasing_or_decreasing_gradually_dampened([52,53,52,51,50], False)
+        result = is_gradual_dampened([52,53,52,51,50])
+        expeced = True
+        self.assertEqual(result,expeced)
+    def test_edcase_1(self):
+        result = is_gradual_dampened([48, 46, 47, 49, 51, 54, 56])
+        expeced = True
+        self.assertEqual(result,expeced)
+    def test_edcase_2(self):
+        result = is_gradual_dampened([1, 1, 2, 3, 4, 5])
+        expeced = True
+        self.assertEqual(result,expeced)
+
+    def test_edcase_3(self):
+        result = is_gradual_dampened([1, 2, 3, 4, 5, 5])
+        expeced = True
+        self.assertEqual(result,expeced)
+
+    def test_edcase_4(self):
+        result = is_gradual_dampened([5, 1, 2, 3, 4, 5])
+        expeced = True
+        self.assertEqual(result,expeced)
+    def test_edcase_5(self):
+        result = is_gradual_dampened([1, 4, 3, 2, 1])
+        expeced = True
+        self.assertEqual(result,expeced)
+
+    def test_edcase_6(self):
+        result = is_gradual_dampened([5,4,5,3,2])
         expeced = True
         self.assertEqual(result,expeced)
     def test_red_nosed_reports_first(self):
